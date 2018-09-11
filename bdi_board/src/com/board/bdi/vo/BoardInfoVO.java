4c8ca58f0294446095a62fc1fdb1bed59e0d8499
koitt-03-P <koitt-03-P@koitt-03-P-PC>
2018-09-11 19:57:53 +0900

    board

--- bdi_board/src/com/board/bdi/vo/BoardInfoVO.java
+++ bdi_board/src/com/board/bdi/vo/BoardInfoVO.java
@@ -37,20 +37,26 @@ public class BoardInfoVO implements Serializable {
 	/** 게시판조회수. */
 	private Integer bicnt;
 
+	@Override
+	public String toString() {
+		return "BoardInfoVO [binum=" + binum + ", bititle=" + bititle + ", bitext=" + bitext + ", bifile=" + bifile
+				+ ", bicredat=" + bicredat + ", bimoddat=" + bimoddat + ", bicnt=" + bicnt + ", biactive=" + biactive
+				+ ", uinum=" + uinum + "]";
+	} 
+
 	/** 게시판활성여부. */
 	private Byte biactive;
+	
+	private Integer uinum;
+	
+	
+	
+	public Integer getUinum() {
+		return uinum;
+	}
 
-	/** 사용자_정보. */
-	private UserInfoVO userInfo;
-
-	/** 댓글정보 목록. */
-	private Set<CommentInfoVO> commentInfoSet;
-
-	/**
-	 * 생성자.
-	 */
-	public BoardInfoVO() {
-		this.commentInfoSet = new HashSet<CommentInfoVO>();
+	public void setUinum(Integer uinum) {
+		this.uinum = uinum;
 	}
 
 	/**
@@ -182,55 +188,7 @@ public class BoardInfoVO implements Serializable {
 	public Byte getBiactive() {
 		return this.biactive;
 	}
-
-	/**
-	 * 사용자_정보을 설정합니다..
-	 * 
-	 * @param userInfo
-	 *            사용자_정보
-	 */
-	public void setUserInfo(UserInfoVO userInfo) {
-		this.userInfo = userInfo;
-	}
-
-	/**
-	 * 사용자_정보을 가져옵니다..
-	 * 
-	 * @return 사용자_정보
-	 */
-	public UserInfoVO getUserInfo() {
-		return this.userInfo;
-	}
-
-	/**
-	 * 댓글정보 목록을 설정합니다..
-	 * 
-	 * @param commentInfoSet
-	 *            댓글정보 목록
-	 */
-	public void setCommentInfoSet(Set<CommentInfoVO> commentInfoSet) {
-		this.commentInfoSet = commentInfoSet;
-	}
-
-	/**
-	 * 댓글정보를 추가합니다..
-	 * 
-	 * @param commentInfo
-	 *            댓글정보
-	 */
-	public void addCommentInfo(CommentInfoVO commentInfo) {
-		this.commentInfoSet.add(commentInfo);
-	}
-
-	/**
-	 * 댓글정보 목록을 가져옵니다..
-	 * 
-	 * @return 댓글정보 목록
-	 */
-	public Set<CommentInfoVO> getCommentInfoSet() {
-		return this.commentInfoSet;
-	}
-
+	
 	/**
 	 * {@inheritDoc}
 	 */
